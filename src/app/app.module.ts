ce56d3dc23bacc097ee0d3ce69671eb6f2196900
PabloRodriguezPUJ <pablo.rodriguezg@javeriana.edu.co>
2023-03-19 01:32:12 -0500

    heder, futer y login creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FuterComponent } from './futer/futer.component';
+import { HederComponent } from './heder/heder.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuterComponent,
+    HederComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
